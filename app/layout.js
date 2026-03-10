49b9973e7467d71aaa701276f3f1a07c5512fb7b
Ricksy114 <eelijahdrex@gmail.com>
2026-03-10 16:14:55 +0800

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "War Thunder Tank Guide",
+  description: "A complete guide to War Thunder ground forces tanks from Tier I to Tier V",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
